5a5bbb12abe3304d9b83683cb0a60d44a2f84821
Anisul <anisulhaque9391@gmail.com>
2023-01-20 00:26:19 -0500

    FEAT: created navbar and added to all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/register" element={<Register />} />
-        <Route path="/register" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
