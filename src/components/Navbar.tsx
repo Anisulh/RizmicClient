7b35b6cc6fa0148e3737a182bf0333aeb5950145
Anisul <anisulhaque9391@gmail.com>
2023-01-24 12:05:35 -0500

    STYLE: Updated navbar to be transparent and conditionally render based on user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,24 @@
-import { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
+import { Fragment, useContext } from "react";
+import { Menu, Popover, Transition } from "@headlessui/react";
 import XMarkIcon from "@heroicons/react/24/outline/XMarkIcon";
 import Bars3Icon from "@heroicons/react/24/outline/Bars3Icon";
 import { Link } from "react-router-dom";
+import { IUserContext, UserContext } from "../UserContext";
+import userAvatar from "../assets/userAvatar.png";
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Navbar() {
+  const { user } = useContext(UserContext) as IUserContext;
   return (
-    <div className="absolute top-0 w-full bg-transparent">
+    <div className="absolute top-0 w-full bg-transparent z-40">
       <Popover className="relative bg-transparent">
         <div className="mx-auto max-w-7xl md:p-0 p-2 bg-transparent ">
           <div className="flex items-center justify-between border-gray-100 my-6 md:justify-start md:space-x-10 bg-transparent">
             <div className="flex justify-start lg:w-0 lg:flex-1 bg-transparent">
-              <Link to="/" className="font-bold text-2xl bg-transparent">
+              <Link to="/" className="font-semibold text-2xl bg-transparent">
                 Rizmic
               </Link>
             </div>
@@ -24,41 +31,110 @@ export default function Navbar() {
                 />
               </Popover.Button>
             </div>
-            <nav className="hidden space-x-10 md:flex bg-transparent">
-              <Link
-                to="/"
-                className="text-base font-medium text-gray-500 hover:text-gray-900 bg-transparent"
-              >
-                Home
-              </Link>
+            {user ? (
+              <>
+                <nav className="hidden space-x-10 md:flex bg-transparent">
+                  <Link
+                    to="/"
+                    className="text-base font-medium text-gray-500 hover:text-gray-900 bg-transparent"
+                  >
+                    Home
+                  </Link>
 
-              <Link
-                to="/wardrobe"
-                className="text-base font-medium text-gray-500 hover:text-gray-900 bg-transparent mix-blend-difference"
-              >
-                Wardrobe
-              </Link>
-              <Link
-                to="/fitgenerator"
-                className="text-base font-medium text-gray-500 hover:text-gray-900 bg-transparent mix-blend-difference"
-              >
-                Fit Generator
-              </Link>
-            </nav>
-            <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0 bg-transparent">
-              <Link
-                to="/login"
-                className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 bg-transparent mix-blend-difference "
-              >
-                Login
-              </Link>
-              <Link
-                to="/register"
-                className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-              >
-                Register
-              </Link>
-            </div>
+                  <Link
+                    to="/wardrobe"
+                    className="text-base font-medium text-gray-500 hover:text-gray-900 bg-transparent mix-blend-difference"
+                  >
+                    Wardrobe
+                  </Link>
+                  <Link
+                    to="/fitgenerator"
+                    className="text-base font-medium text-gray-500 hover:text-gray-900 bg-transparent mix-blend-difference"
+                  >
+                    Fit Generator
+                  </Link>
+                </nav>
+                <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0 bg-transparent">
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="flex rounded-full text-sm focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 focus:ring-offset-gray-800 border">
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="h-8 w-8 rounded-full "
+                          src={user.profilePicture ?? userAvatar}
+                          alt=""
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Your Profile
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Settings
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Log out
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              </>
+            ) : (
+              <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0 bg-transparent">
+                <Link
+                  to="/login"
+                  className="whitespace-nowrap text-base font-medium text-raisinblackhover:text-gray-900 bg-transparent mix-blend-difference "
+                >
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-ultramarineBlue px-4 py-2 text-base font-medium text-white shadow-sm "
+                >
+                  Register
+                </Link>
+              </div>
+            )}
           </div>
         </div>
 
