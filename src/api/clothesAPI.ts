afd575850752cf21362017f98305758f079b257e
anisulh <anisulhaque9391@gmail.com>
2023-02-04 10:18:41 -0500

    FEAT: added additional functions to handle update and delete

--- src/api/clothesAPI.ts
+++ src/api/clothesAPI.ts
@@ -1,10 +1,9 @@
-import { IClothingData } from "../components/ClothesModal";
+import { IClothingData, IUpdateClothingData } from "../components/ClothesModal";
 
 const baseURL = "http://localhost:7000/clothes/";
 
 export const getClothes = async (token: string | undefined) => {
   if (!token) return;
-  console.log(token);
   const options = {
     method: "GET",
     headers: {
@@ -14,7 +13,7 @@ export const getClothes = async (token: string | undefined) => {
   };
 
   const response = await fetch(baseURL, options);
-  return response.json();
+  return response;
 };
 
 export const createClothes = async (
@@ -31,5 +30,34 @@ export const createClothes = async (
   };
 
   const response = await fetch(baseURL, options);
+  return response.json();
+};
+
+export const updateClothes = async (clothesId: string, clothingData:IUpdateClothingData, token: string) => {
+  const url = new URL(clothesId, baseURL);
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(clothingData)
+  };
+
+  const response = await fetch(url, options);
+  return response.json();
+};
+
+export const deleteClothes = async (clothesId: string, token: string) => {
+  const url = new URL(clothesId, baseURL);
+  const options = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await fetch(url, options);
   return response.json();
 };
