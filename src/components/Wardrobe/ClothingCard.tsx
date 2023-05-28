7c04529e5c8d0797cbf43576b7d6d0b9c181714e
Anisul <anisulhaque9391@gmail.com>
2023-05-28 11:01:53 -0400

    FIX: menu appearing behind other components

--- src/components/Wardrobe/ClothingCard.tsx
+++ src/components/Wardrobe/ClothingCard.tsx
@@ -83,7 +83,7 @@ export default function ClothingCard({
                   leaveFrom="transform opacity-100 scale-100"
                   leaveTo="transform opacity-0 scale-95"
                 >
-                  <Menu.Items className="absolute right-0  w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white  shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Items className="absolute right-0  w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white  shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-20">
                     <div className="px-1 py-1 ">
                       <Menu.Item>
                         {({ active }) => (
