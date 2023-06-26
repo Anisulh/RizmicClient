a4b79119f1ab10d01755fb50d42712ee9877d268
Anisul <anisulhaque9391@gmail.com>
2023-06-26 19:56:08 -0400

    FEAT: added server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port : 3000,
+    proxy: {
+        '/api': {
+            target: 'http://localhost:7001',
+            changeOrigin: true,
+            secure: false
+        }
+    }
+    
+},
 })
