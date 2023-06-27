6e480cb2b62055fc2261be70d0672be4825d121e
Anisul <anisulhaque9391@gmail.com>
2023-06-26 21:16:59 -0400

    REFACTOR: removed proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port : 3000,
-    proxy: {
-        '/api': {
-            target: 'http://localhost:7001',
-            changeOrigin: true,
-            secure: false
-        }
-    }
-    
-},
 })
