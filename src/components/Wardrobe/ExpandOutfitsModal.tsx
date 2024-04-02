e3480eb6416ed0c0ceaca937ccea8b58af37f8a2
Anisul <anisulhaque9391@gmail.com>
2024-04-02 03:56:30 -0400

    Update background color in ExpandOutfitsModal component

--- src/components/Wardrobe/ExpandOutfitsModal.tsx
+++ src/components/Wardrobe/ExpandOutfitsModal.tsx
@@ -51,7 +51,7 @@ export default function ExpandOutfitsModal({
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className=" relative w-fit transform  rounded-2xl bg-sWhite p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className=" relative w-fit transform  rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
