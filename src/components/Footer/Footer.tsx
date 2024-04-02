41b9774474df4f25de6980fc9e08f3f2be2e03be
Anisul <anisulhaque9391@gmail.com>
2024-04-02 03:56:04 -0400

    Refactor Footer component styling and links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,46 +1,29 @@
-import { Link } from "react-router-dom"
-import './footer.css'
+import { Link } from "react-router-dom";
+import "./footer.css";
 
 function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-sWhite to-cambridgeblue py-6 mt-12 footer--pin">
-    {/* <ul className="social-links">
-      <li>
-        <a href="#fds">
-          <img src="facebook-icon.png" alt="Facebook" />
-        </a>
-      </li>
-      <li>
-        <a href="#fds">
-          <img src="twitter-icon.png" alt="Twitter" />
-        </a>
-      </li>
-      <li>
-        <a href="#fds">
-          <img src="instagram-icon.png" alt="Instagram" />
-        </a>
-      </li>
-    </ul> */}
-    <div className="flex flex-col justify-start gap-y-1 px-32 text-sm text-raisinblack">
-      <div className="text-xl font-semibold">Site Links</div>
-      <Link
-        className="max-w-[fit-content] min-w-[fit-content]"
-        to="/aboutus"
-      >
-        About Us
-      </Link>
-      <Link
-        className="max-w-[fit-content] min-w-[fit-content]"
-        to="/contactus"
-      >
-        Contact Us
-      </Link>
-    </div>
-    <div className="container mx-auto px-4 text-center text-raisinblack">
-      &copy; {new Date().getFullYear()} Rizmic. All rights reserved.
-    </div>
-  </footer>
-  )
+    <footer className="bg-gradient-to-r from-sWhite to-cambridgeblue py-6">
+      <div className="flex flex-col justify-start gap-y-1 px-4 sm:px-8 md:px-16 lg:px-32 text-sm text-raisinblack">
+        <div className="text-xl font-semibold">Site Links</div>
+        <Link
+          className="max-w-[fit-content] min-w-[fit-content]"
+          to="/about-us"
+        >
+          About Us
+        </Link>
+        <Link
+          className="max-w-[fit-content] min-w-[fit-content]"
+          to="/contact-us"
+        >
+          Contact Us
+        </Link>
+      </div>
+      <div className="container mx-auto px-4 text-center text-raisinblack">
+        &copy; {new Date().getFullYear()} RizmicFits. All rights reserved.
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
