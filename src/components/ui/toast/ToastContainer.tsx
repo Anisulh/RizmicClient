2c56b529b65c46adaa9d6f6f4fc735c1b8b309dc
ubergoose <anisulhaque9391@gmail.com>
2024-04-21 09:41:49 -0400

    Refactor ToastContainer component to use removeToast function in Toast component

--- src/components/ui/toast/ToastContainer.tsx
+++ src/components/ui/toast/ToastContainer.tsx
@@ -1,13 +1,17 @@
-import { useToast } from "../../../contexts/ToastContext";
 import Toast from "./Toast";
+import { useToast } from "../../../contexts/ToastContext";
 
 const ToastContainer = () => {
-  const { toasts } = useToast();
+  const { toasts, removeToast } = useToast();
 
   return (
     <div className="fixed top-0 right-0 p-4 space-y-2 z-50">
       {toasts.map((toast) => (
-        <Toast key={toast.id} {...toast} />
+        <Toast
+          key={toast.id}
+          {...toast}
+          removeToast={() => removeToast(toast.id)}
+        />
       ))}
     </div>
   );
