4e1a5557247156698f73b56b25a9fea6761cde52
ubergoose <anisulhaque9391@gmail.com>
2024-05-01 18:00:52 -0400

    Add outfit query logic , organize clothing data for clothing section, add spinner

--- src/pages/Wardrobe.tsx
+++ src/pages/Wardrobe.tsx
@@ -1,18 +1,39 @@
 import { useState } from "react";
 import ClothesSection from "../components/Wardrobe/ClothesSection";
-import OutiftSection from "../components/Wardrobe/OutiftSection";
 import { useQuery } from "@tanstack/react-query";
 import { getClothes } from "../api/clothesAPI";
 import { useToast } from "../contexts/ToastContext";
 import { PlusIcon } from "@heroicons/react/20/solid";
 import { IExistingClothesData } from "../components/Wardrobe/ClothesModal";
+import Button from "../components/ui/Button";
+import Spinner from "../components/ui/spinner/Spinner";
+import { IExistingOutfitData } from "../components/Wardrobe/OutfitsModal";
+import { getOutfits } from "../api/outfitsAPI";
+import OutfitSection from "../components/Wardrobe/OutiftSection";
+
+type GroupedClothes = {
+  [key in IExistingClothesData["category"]]?: IExistingClothesData[];
+};
+export interface IOutfitsSections {
+  favoriteOutfits: IExistingOutfitData[];
+  allOutfits: IExistingOutfitData[];
+}
 
 export default function Wardrobe() {
   const [openTab, setOpenTab] = useState(1);
-  const [clothes, setClothes] = useState<{ [key: string]: IExistingClothesData[] }>({});
+  const [structuredClothes, setStructuredClothes] = useState<{
+    [key: string]: IExistingClothesData[];
+  }>({});
+  const [unstructuredClothes, setUnstructuredClothes] = useState<
+    IExistingClothesData[]
+  >([]);
+  const [outfits, setOutfits] = useState<IOutfitsSections>({
+    favoriteOutfits: [],
+    allOutfits: [],
+  });
   const [modalOpen, setModalOpen] = useState(false);
   const { addToast } = useToast();
-  const { isLoading, refetch } = useQuery({
+  const { isLoading: isLoadingClothes, refetch: refetchClothes } = useQuery({
     queryKey: ["clothes"],
     queryFn: async () => {
       const data = await getClothes();
@@ -23,22 +44,62 @@ export default function Wardrobe() {
           type: "error",
         });
       } else {
-        console.log(data)
-        setClothes(data);
+        setUnstructuredClothes(data);
+        const groupedByCategory = data.reduce(
+          (acc: GroupedClothes, cloth: IExistingClothesData) => {
+            const categoryArray = acc[cloth.category] || [];
+            categoryArray.push(cloth);
+            acc[cloth.category] = categoryArray;
+
+            return acc;
+          },
+          {},
+        );
+        setStructuredClothes(groupedByCategory);
+        return data;
+      }
+    },
+    refetchOnWindowFocus: false,
+  });
+  const { isLoading: isLoadingOutfits, refetch: refetchOutfits } = useQuery({
+    queryKey: ["outfits"],
+    queryFn: async () => {
+      const data = await getOutfits();
+      if (data?.message) {
+        addToast({
+          title: "Something went wrong.",
+          description: data?.message,
+          type: "error",
+        });
+      } else {
+        const tempFav: IExistingOutfitData[] = [];
+        data.outfits.map((item: IExistingOutfitData) => {
+          if (item.favorited) {
+            tempFav.push(item);
+          }
+        });
+        setOutfits({
+          favoriteOutfits: tempFav,
+          allOutfits: data.outfits,
+        });
         return data;
       }
     },
     refetchOnWindowFocus: false,
   });
 
+  if (isLoadingClothes || isLoadingOutfits) {
+    return <Spinner />;
+  }
+
   return (
     <div className="content-container max-w-7xl w-full mb-10 relative mx-auto">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between px-4">
         <ul className="flex items-center pt-3 pb-4" role="tablist">
           <li className=" mr-2 text-center">
             <button
               className={
-                "text-xs font-bold uppercase px-5 py-3 block leading-normal " +
+                "text-xs font-bold uppercase pr-5 py-3 block leading-normal " +
                 (openTab === 1 && "text-cambridgeblue")
               }
               onClick={(e) => {
@@ -55,7 +116,7 @@ export default function Wardrobe() {
           <li className=" mr-2 text-center">
             <button
               className={
-                "text-xs font-bold uppercase px-5 py-3 block leading-normal " +
+                "text-xs font-bold uppercase pl-5 py-3 block leading-normal " +
                 (openTab === 2 && "text-cambridgeblue")
               }
               onClick={(e) => {
@@ -70,30 +131,40 @@ export default function Wardrobe() {
           </li>
         </ul>
 
-        <button
+        <Button
+          variant="textWithIcon"
+          icon={<PlusIcon className="h-6 w-6" />}
+          onClick={() => setModalOpen(true)}
+          className="p-2 hover:bg-cambridgeblue"
+        >
+          Add Item
+        </Button>
+
+        {/* <button
           className="flex items-center bg-ultramarineBlue text-white hover:bg-cambridgeblue hover:text-black rounded-lg px-2 py-1 transition-all"
           onClick={() => setModalOpen(true)}
         >
           Add item
           <PlusIcon className="h-6 w-6" />
-        </button>
+        </button> */}
       </div>
 
       {openTab === 1 ? (
         <>
           <ClothesSection
-            clothes={clothes}
-            isLoading={isLoading}
-            refetch={refetch}
+            clothes={structuredClothes}
+            refetch={refetchClothes}
             modalOpen={modalOpen}
             setModalOpen={setModalOpen}
           />
         </>
       ) : (
-        <OutiftSection
-          clothes={clothes}
+        <OutfitSection
+          outfits={outfits}
+          clothes={unstructuredClothes}
           modalOpen={modalOpen}
           setModalOpen={setModalOpen}
+          refetch={refetchOutfits}
         />
       )}
     </div>
