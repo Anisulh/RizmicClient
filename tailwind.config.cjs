86f5c44a17f31677e58e88168693b6347f3c60a8
ubergoose <anisulhaque9391@gmail.com>
2024-05-07 22:23:01 -0400

    feat: Enable dark mode in tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
+  darkMode: 'media',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
