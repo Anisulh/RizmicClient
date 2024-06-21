13f10c01488ef04231c1a884f4480fda30d6e050
ubergoose <anisulhaque9391@gmail.com>
2024-06-21 16:33:25 -0400

    Update mobile nav bar and add install button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,6 +6,8 @@ import { useAuth } from "../../contexts/UserContext";
 import userAvatar from "../../assets/userAvatar.webp";
 import RizmicIcon from "../../assets/RFIcon.png";
 import XMarkIcon from "@heroicons/react/24/outline/XMarkIcon";
+import useInstallPrompt from "../../hooks/useInstallPrompt";
+import Button from "../ui/Button";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -23,6 +25,7 @@ const loginRegisterNav = [
 
 export default function Navbar() {
   const { user, isAuthenticated, logout } = useAuth();
+  const { isInstallPromptVisible, showInstallPrompt } = useInstallPrompt();
   const navigate = useNavigate();
   const location = useLocation();
   const isHomePage = location.pathname === "/";
@@ -42,7 +45,7 @@ export default function Navbar() {
         <>
           <div className="relative">
             <div className="mx-auto max-w-7xl px-2 lg:px-4">
-              <div className="flex items-center justify-between border-gray-100 my-4 lg:my-6 md:justify-start md:space-x-10">
+              <div className="flex items-center justify-between border-gray-100 dark:text-gray-900 my-4 lg:my-6 md:justify-start md:space-x-10">
                 <Link
                   to="/"
                   className="font-semibold text-2xl hover:bg-transparent flex gap-1 lg:flex-1"
@@ -54,15 +57,144 @@ export default function Navbar() {
                   />
                   <span className="hidden md:block">RizmicFits</span>
                 </Link>
-                <Disclosure.Button className=" md:hidden relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
+                <Menu as="div" className="relative">
+                  <Menu.Button className=" md:hidden relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="absolute -inset-0.5" />
+                    <span className="sr-only">Open main menu</span>
+                    {isInstallPromptVisible && (
+                      <Button onClick={showInstallPrompt}>Install App</Button>
+                    )}
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Menu.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="md:hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-slate-600 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      {isAuthenticated ? (
+                        <div className="space-y-1 px-2 pb-3 pt-2 shadow-md">
+                          {["/wardrobe", "/generate-fit"].map((path, index) => (
+                            <Menu.Item key={path}>
+                              {({ active }) => (
+                                <Link
+                                  to={path}
+                                  className={classNames(
+                                    active
+                                      ? "bg-gray-100 dark:text-gray-900"
+                                      : "",
+                                    "block px-4 py-2 text-sm  text-gray-900 dark:text-white hover:text-gray-700",
+                                  )}
+                                >
+                                  {index === 0 ? "Wardrobe" : "Fit Generator"}
+                                </Link>
+                              )}
+                            </Menu.Item>
+                          ))}
+                          <Menu.Item>
+                            {({ active }) => (
+                              <button
+                                onClick={() => {
+                                  logout();
+                                  navigate("/login");
+                                }}
+                                className={classNames(
+                                  active
+                                    ? "bg-gray-100 dark:text-gray-900"
+                                    : "",
+                                  "block px-4 py-2 text-sm font-medium text-gray-900 dark:text-white hover:text-gray-700",
+                                )}
+                              >
+                                Log out
+                              </button>
+                            )}
+                          </Menu.Item>
+                        </div>
+                      ) : isHomePage ? (
+                        <div className="space-y-1 px-2 pb-3 pt-2 shadow-md">
+                          {homeNavigation.map((item, index) => (
+                            <Menu.Item key={index}>
+                              {({ active }) => (
+                                <Link
+                                  to={item.href}
+                                  className={classNames(
+                                    active
+                                      ? "bg-gray-100 dark:text-gray-900"
+                                      : "",
+                                    "block px-4 py-2 text-sm  text-gray-900 dark:text-white hover:text-gray-700",
+                                  )}
+                                >
+                                  {item.name}
+                                </Link>
+                              )}
+                            </Menu.Item>
+                          ))}
+                          {loginRegisterNav.map((item, index) => (
+                            <Menu.Item key={index}>
+                              {({ active }) => (
+                                <Link
+                                  to={item.href}
+                                  className={classNames(
+                                    active
+                                      ? "bg-gray-100 dark:text-gray-900"
+                                      : "",
+                                    "block px-4 py-2 text-sm  text-gray-900 dark:text-white hover:text-gray-700",
+                                  )}
+                                >
+                                  {item.name}
+                                </Link>
+                              )}
+                            </Menu.Item>
+                          ))}
+                        </div>
+                      ) : (
+                        <div className="space-y-1 px-2 pb-3 pt-2 shadow-md">
+                          {loginRegisterNav.map((item, index) => (
+                            <Menu.Item key={index}>
+                              {({ active }) => (
+                                <Link
+                                  to={item.href}
+                                  className={classNames(
+                                    active
+                                      ? "bg-gray-100 dark:text-gray-900"
+                                      : "",
+                                    "block px-4 py-2 text-sm  text-gray-900 dark:text-white hover:text-gray-700",
+                                  )}
+                                >
+                                  {item.name}
+                                </Link>
+                              )}
+                            </Menu.Item>
+                          ))}
+                        </div>
+                      )}
+                      {isInstallPromptVisible && (
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              className={classNames(
+                                active ? "bg-gray-100 dark:text-gray-900" : "",
+                                "flex  items-center justify-center rounded-md border border-transparent bg-ultramarineBlue px-8  py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700",
+                              )}
+                              onClick={showInstallPrompt}
+                            >
+                              Install App
+                            </button>
+                          )}
+                        </Menu.Item>
+                      )}
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
 
                 {isAuthenticated ? (
                   <>
@@ -102,12 +234,14 @@ export default function Navbar() {
                           leaveTo="transform opacity-0 scale-95"
                         >
                           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-slate-600 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          <Menu.Item>
+                            <Menu.Item>
                               {({ active }) => (
                                 <Link
                                   to="/friends"
                                   className={classNames(
-                                    active ? "bg-gray-100" : "",
+                                    active
+                                      ? "bg-gray-100 dark:text-gray-900"
+                                      : "",
                                     "block px-4 py-2 text-sm  text-gray-900 dark:text-white hover:text-gray-700",
                                   )}
                                 >
@@ -120,7 +254,9 @@ export default function Navbar() {
                                 <Link
                                   to="/profile"
                                   className={classNames(
-                                    active ? "bg-gray-100" : "",
+                                    active
+                                      ? "bg-gray-100 dark:text-gray-900"
+                                      : "",
                                     "block px-4 py-2 text-sm  text-gray-900 dark:text-white hover:text-gray-700",
                                   )}
                                 >
@@ -133,7 +269,9 @@ export default function Navbar() {
                                 <Link
                                   to="/settings"
                                   className={classNames(
-                                    active ? "bg-gray-100" : "",
+                                    active
+                                      ? "bg-gray-100 dark:text-gray-900"
+                                      : "",
                                     "block px-4 py-2 text-sm  text-gray-900 dark:text-white hover:text-gray-700",
                                   )}
                                 >
@@ -150,7 +288,9 @@ export default function Navbar() {
                                       navigate("/login");
                                     }}
                                     className={classNames(
-                                      active ? "bg-gray-100" : "",
+                                      active
+                                        ? "bg-gray-100 dark:text-gray-900"
+                                        : "",
                                       "flex  items-center justify-center rounded-md border border-transparent bg-ultramarineBlue px-8  py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700",
                                     )}
                                   >
@@ -185,7 +325,7 @@ export default function Navbar() {
                   <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
                     <Link
                       to="/login"
-                      className="text-gray-800 dark:text-gray-800 hover:text-gray-900 dark:hover:text-gray-900 mix-blend-difference"
+                      className="dark:text-gray-800 hover:text-gray-900 mix-blend-difference"
                     >
                       Login
                     </Link>
@@ -200,33 +340,6 @@ export default function Navbar() {
               </div>
             </div>
           </div>
-          {!user && (
-            <Disclosure.Panel className="md:hidden bg-white rounded-lg">
-              <div className="space-y-1 px-2 pb-3 pt-2 shadow-md">
-                {isHomePage &&
-                  homeNavigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className="text-gray-900 hover:bg-ultramarineBlue hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                {loginRegisterNav.map((item) => (
-                  <Disclosure.Button
-                    key={item.name}
-                    as="a"
-                    href={item.href}
-                    className="text-gray-900 hover:bg-ultramarineBlue hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-                  >
-                    {item.name}
-                  </Disclosure.Button>
-                ))}
-              </div>
-            </Disclosure.Panel>
-          )}
         </>
       )}
     </Disclosure>
