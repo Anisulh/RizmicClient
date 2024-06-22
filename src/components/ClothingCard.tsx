cd5c64b91436149b994adc0bb12663d961276476
ubergoose <anisulhaque9391@gmail.com>
2024-06-22 00:07:37 -0400

    Replace category with clothing name

--- src/components/ClothingCard.tsx
+++ src/components/ClothingCard.tsx
@@ -108,7 +108,7 @@ export default function ClothingCard({
 
           <div className="flex justify-between w-full mt-1">
             <div>
-              <h3>{category.charAt(0).toUpperCase() + category.slice(1)}</h3>
+              <h3>{item.name}</h3>
               <p className="text-slategrey text-sm">
                 Color: {color.charAt(0).toUpperCase() + color.slice(1)}
               </p>
@@ -157,6 +157,31 @@ export default function ClothingCard({
                               />
                             )}
                             Edit
+                          </button>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            onClick={() => setEditMenuOpen(true)}
+                            className={`${
+                              active
+                                ? "bg-ultramarineBlue text-white"
+                                : "text-gray-900"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            {active ? (
+                              <EditActiveIcon
+                                className="mr-2 h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            ) : (
+                              <EditInactiveIcon
+                                className="mr-2 h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            )}
+                            Share
                           </button>
                         )}
                       </Menu.Item>
