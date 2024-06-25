39a0fc1fa727f9cd336af91c89b5be3594bec1de
ubergoose <anisulhaque9391@gmail.com>
2024-06-25 10:46:08 -0400

    Add unfriend modal for confimation, moved unfriend button to menu, updated styling

--- src/pages/friends/Friends.tsx
+++ src/pages/friends/Friends.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import {
   acceptFriendRequestAPI,
   getFriendRequests,
@@ -11,14 +11,18 @@ import AddFriendModal from "./components/AddFriendModal";
 import { useToast } from "../../contexts/ToastContext";
 import {
   CheckIcon,
+  EllipsisVerticalIcon,
   MagnifyingGlassIcon,
   XMarkIcon,
 } from "@heroicons/react/20/solid";
 import useFriends from "../../hooks/useFriends";
+import { Menu, Transition } from "@headlessui/react";
+import DialogModal from "../../components/ui/modal/DialogModal";
 
 export default function Friends() {
   const { addToast } = useToast();
   const [addFriendModalOpen, setAddFriendModalOpen] = useState(false);
+  const [unfriendModalOpen, setUnfriendModalOpen] = useState(false);
 
   const {
     isPending: friendQueryPending,
@@ -79,7 +83,7 @@ export default function Friends() {
         <Button
           variant="ghost"
           onClick={() => setAddFriendModalOpen(true)}
-          className="flex gap-2 p-1 hover:bg-slate-600"
+          className="flex gap-2 p-1 hover:bg-cambridgeblue dark:hover:text-slate-700"
         >
           <p className="hidden md:block">Search</p>
           <MagnifyingGlassIcon className="size-6" />
@@ -90,7 +94,7 @@ export default function Friends() {
         {friends?.map((friend) => (
           <div
             key={friend._id}
-            className="flex items-center justify-between rounded-xl bg-slate-600 p-4"
+            className="flex items-center justify-between rounded-xl bg-slate-700 p-4"
           >
             <div className="flex items-center gap-2">
               {friend.profilePicture && (
@@ -106,14 +110,78 @@ export default function Friends() {
                 </h2>
               </div>
             </div>
-            <div className="space-x-2">
-              <Button
-                variant="destructive"
-                onClick={() => handleUnfriend(friend._id)}
-              >
-                Unfriend
-              </Button>
+            <div>
+              <Menu as="div" className="relative inline-block text-right">
+                <div>
+                  <Menu.Button className="inline-flex w-full justify-center rounded-md text-right text-sm font-medium text-raisinblack hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                    <EllipsisVerticalIcon
+                      className="size-6 transition-colors hover:text-cambridgeblue dark:text-white"
+                      aria-hidden="true"
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-20 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-slate-600 dark:text-gray-200">
+                    <div className="px-1 py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={`${
+                              active && "bg-ultramarineBlue"
+                            } group flex w-full items-center rounded-md px-4 py-2 transition-colors`}
+                          >
+                            View Profile
+                          </button>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            onClick={() => setUnfriendModalOpen(true)}
+                            className={`${
+                              active && "bg-red-600"
+                            } group flex w-full items-center rounded-md px-4 py-2 transition-colors`}
+                          >
+                            Unfriend
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
             </div>
+            <DialogModal
+              title="Delete Clothing"
+              open={unfriendModalOpen}
+              setOpen={setUnfriendModalOpen}
+            >
+              <div className="my-4 flex flex-col items-center justify-center space-y-4">
+                <p>Are you sure you want to unfriend this user? </p>
+                <div className="flex w-full items-center justify-between">
+                  <Button
+                    variant="destructive"
+                    onClick={() => {
+                      handleUnfriend(friend._id);
+                      setUnfriendModalOpen(false);
+                    }}
+                  >
+                    Yes
+                  </Button>
+                  <Button onClick={() => setUnfriendModalOpen(false)}>
+                    No
+                  </Button>
+                </div>
+              </div>
+            </DialogModal>
           </div>
         ))}
       </div>
@@ -162,7 +230,7 @@ export default function Friends() {
       <AddFriendModal
         open={addFriendModalOpen}
         setOpen={setAddFriendModalOpen}
-      />
+      />{" "}
     </div>
   );
 }
