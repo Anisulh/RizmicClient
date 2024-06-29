d9a2b1e45a64bcaeb060c7283778f95cecc40736
ubergoose <anisulhaque9391@gmail.com>
2024-06-29 12:47:11 -0400

    disable query if user state is populated

--- src/hooks/useFetchUser.tsx
+++ src/hooks/useFetchUser.tsx
@@ -2,12 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { useAuth } from "../contexts/UserContext";
 
 export const useFetchUser = () => {
-  const { refetchUserData } = useAuth();
+  const { refetchUserData, user } = useAuth();
   return useQuery({
     queryKey: ["user"],
     queryFn: refetchUserData,
     refetchOnWindowFocus: false,
     retry: false,
     staleTime: 5 * 60 * 1000,
+    enabled: !user,
   });
 };
