1637ed42ccf4c9a89072552b9135a7eb3750aeca
ubergoose <anisulhaque9391@gmail.com>
2024-06-29 02:14:55 -0400

    added reload dialog modal and online status toast and moved toast provider and container

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Register from "./pages/register/Register";
 import AboutUs from "./pages/aboutUs/AboutUs";
 import ContactUs from "./pages/contactUs/ContactUs";
 import ForgotPassword from "./pages/forgotPassword/ForgotPassword";
-import { ToastProvider } from "./contexts/ToastContext";
 import PasswordReset from "./pages/passwordReset/PasswordReset";
 import Wardrobe from "./pages/wardrobe/Wardrobe";
 import PrivateRoute from "./components/PrivateRoute";
@@ -16,20 +15,71 @@ import GenerateFit from "./pages/fitGenerator/GenerateFit";
 import Profile from "./pages/profile/Profile";
 import Footer from "./components/Footer/Footer";
 import Pricing from "./pages/subscription/Pricing";
-import ToastContainer from "./components/ui/toast/ToastContainer";
 import PrivacyPolicy from "./pages/privacyPolicy/PrivacyPolicy";
 import TermsOfService from "./pages/termsOfService/TermsOfService";
 import Settings from "./pages/settings/Settings";
 import Friends from "./pages/friends/Friends";
 import ClothingItem from "./pages/clothingItem/ClothingItem";
 import OutfitItem from "./pages/outfitItem/OutfitItem";
+import { useRegisterSW } from "virtual:pwa-register/react";
+import { useEffect, useState } from "react";
+import { useToast } from "./contexts/ToastContext";
+import DialogModal from "./components/ui/modal/DialogModal";
+import Button from "./components/ui/Button";
 
 const queryClient = new QueryClient();
 
 function App() {
+  const { addToast } = useToast();
+  const [_, setOnline] = useState(navigator.onLine);
+  const [wasOffline, setWasOffline] = useState(false);
+
+  const {
+    needRefresh: [needRefresh, setNeedRefresh],
+    updateServiceWorker,
+  } = useRegisterSW({
+    onRegistered(r) {
+      console.log("SW Registered:", r);
+    },
+    onRegisterError(error) {
+      console.log("SW registration error", error);
+    },
+  });
+
+  useEffect(() => {
+    const handleOffline = () => {
+      setOnline(false);
+      setWasOffline(true);
+      addToast({
+        title: "No internet connection",
+        description: "Please check your internet connection.",
+        type: "warning",
+      });
+    };
+
+    const handleOnline = () => {
+      setOnline(true);
+      if (wasOffline) {
+        addToast({
+          title: "Internet connection restored",
+          description: "You are now connected to the internet.",
+          type: "success",
+        });
+      }
+    };
+
+    window.addEventListener("offline", handleOffline);
+    window.addEventListener("online", handleOnline);
+
+    return () => {
+      window.removeEventListener("offline", handleOffline);
+      window.removeEventListener("online", handleOnline);
+    };
+  }, [addToast, wasOffline]);
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <ToastProvider>
+    <div>
+      <QueryClientProvider client={queryClient}>
         <Router>
           <Navbar />
           <Routes>
@@ -91,10 +141,23 @@ function App() {
           </Routes>
           <Footer />
         </Router>
-        <ToastContainer />
-      </ToastProvider>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+      <DialogModal
+        title="New Update!"
+        open={needRefresh}
+        setOpen={setNeedRefresh}
+      >
+        <div className="mt-4 space-y-4">
+          <p>
+            Got some more improvements we hope you&apos;ll enjoy :)
+            <br />
+            Click on reload button to update.
+          </p>
+          <Button onClick={() => updateServiceWorker(true)}>Reload</Button>
+        </div>
+      </DialogModal>
+    </div>
   );
 }
 
