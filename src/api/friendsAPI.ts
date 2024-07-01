c58deb20bfc308a6564a298a332788f81c6024e7
ubergoose <anisulhaque9391@gmail.com>
2024-07-01 11:50:34 -0400

    add share clothes and get friend profile api calls, add additional types

--- src/api/friendsAPI.ts
+++ src/api/friendsAPI.ts
@@ -1,3 +1,5 @@
+import { IExistingClothesData } from "../pages/wardrobe/components/ClothesModal";
+import { IExistingOutfitData } from "../pages/wardrobe/components/OutfitsModal";
 import { getFriendsCache, setFriendsCache } from "../utils/indexDB";
 
 const baseURL = `${import.meta.env.VITE_BASE_URL}/friends/`;
@@ -7,6 +9,24 @@ export interface IFriend {
   firstName: string;
   lastName: string;
   profilePicture: string;
+}
+
+export interface ISharedClothing {
+  _id: string;
+  sharedBy: string;
+  sharedWith: string;
+  clothingItem: IExistingClothesData;
+  status: string;
+  sharedAt: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: 0;
+}
+
+export interface IFriendProfile extends IFriend {
+  since: string;
+  sharedClothes: ISharedClothing[];
+  sharedOutfits: IExistingOutfitData[];
 }
 
 export interface IFriendRequest {
@@ -99,6 +119,53 @@ export const unfriendAPI = async (userId: string) => {
   const url = new URL(baseURL + `unfriend/${userId}`);
   const options: RequestInit = {
     method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  };
+  const response = await fetch(url, options);
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
+  return data;
+};
+
+export const shareClothesWithFriends = async (
+  clothesId: string,
+  friends: string[],
+) => {
+  const url = new URL(baseURL + `share/clothing/${clothesId}`);
+  const options: RequestInit = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ friends }),
+  };
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchFriendProfile = async (
+  userId: string | undefined,
+): Promise<IFriendProfile> => {
+  if (!userId) {
+    throw new Error("User ID is required");
+  }
+  const url = new URL(baseURL + `profile/${userId}`);
+  const options: RequestInit = {
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
